1afeb61c281e1c4f59f0d9f1a7badf23a0c32fc9
Vadim V.Kuznetsov <mac-t@yandex.ru>
2022-02-17 12:40:39 +0300

    Standard checks

--- Watchdog.pm
+++ Watchdog.pm
@@ -1,7 +1,9 @@
-package Fwatch;
+package Watchdog;
 
-use POE;
+use Time::HiRes;
 use Fcntl qw(:flock :seek);
+
+use Data::Dumper;
 
 sub new {
 	my $class = shift;
@@ -10,13 +12,43 @@ sub new {
 
 	my $self = bless( $init, $class);
 
-	die "Cannot read" unless -f( $init->{'filename'}) && -r( $init->{'filename'});
-	
-	$self->{'fh'} = open( "< $init->{'filename'}");
+	die "Cannot read $init->{'filename'}" unless -f( $init->{'filename'}) && -r( $init->{'filename'});
+
+	open( $self->{'fh'}, "< $init->{'filename'}") || die "Cannot open $init->{'filename'}";
+
+	$self->content;
 	return $self;
 }
-
+#############
+sub size {	#	Optional functionality
+#############
+	my $self = shift;
+	$self->content;
+	return $self->{'size'};
+}
+#############
+sub content {
+#############
+	my $self = shift;
+	my $stat = [stat( $self->{'fh'})];
+	if ( $self->{'mtime'} != $stat->[9] ) {
+		$self->{'mtime'} = $stat->[9];
+		$self->{'size'} = $stat->[7];
+		$self->{'blksize'} = $stat->[10];
+		seek( $self->{'fh'}, 0, SEEK_SET);
+		sysread( $self->{'fh'}, $self->{'content'}, $self->{'blksize'});
+	}
+	return $self->{'content'};
+}
+#############
+sub close {
+#############
+	my $self = shift;
+	return $self->DESTROY;
+}
+#############
 sub DESTROY {
+#############
 	my $self = shift;
 	close( $self->{'fh'});
 	return undef $self;
--- fwatch.pl
+++ fwatch.pl
@@ -5,4 +5,10 @@ use Encode;
 use 5.18.0;
 use Watchdog;
 
-my $guard = Watchdog->new( filename => './file.txt' );
+my $guard = Watchdog->new( filename => 'file.txt' );	# '/var/www/atk/log/drive.log'
+
+while (1) {
+	my $content = $guard->content;
+	say $content;
+	sleep(1);
+}
